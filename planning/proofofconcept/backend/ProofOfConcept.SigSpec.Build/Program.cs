b30f5f4f349c7252f13d8591646f0580cf6dbdf8
DubyaDude <ushafiq141@gmail.com>
2023-10-10 22:19:30 -0400

    Added sigspec generation for signalr

---
+++ planning/proofofconcept/backend/ProofOfConcept.SigSpec.Build/Program.cs
@@ -0,0 +1,31 @@
+﻿using ProofOfConcept.Hubs;
+using SigSpec.CodeGeneration.TypeScript;
+using SigSpec.Core;
+
+namespace ProofOfConcept.SigSpec.Build
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            var settings = new SigSpecGeneratorSettings();
+            var generator = new SigSpecGenerator(settings);
+
+            // TODO: Add PR to SignalR Core with new IHubDescriptionCollectionProvider service
+            var document = await generator.GenerateForHubsAsync(ProofOfConcept.Program.HubList);
+
+            var json = document.ToJson();
+            Console.WriteLine("\nGenerated SigSpec document:");
+            Console.WriteLine(json);
+            Console.ReadKey();
+
+            var tsCodeGeneratorSettings = new SigSpecToTypeScriptGeneratorSettings();
+            var tsCodeGenerator = new SigSpecToTypeScriptGenerator(tsCodeGeneratorSettings);
+            var file = tsCodeGenerator.GenerateFile(document);
+
+            Console.WriteLine("\n\nGenerated SigSpec TypeScript code:");
+            Console.WriteLine(file);
+            Console.ReadKey();
+        }
+    }
+}
--- planning/proofofconcept/backend/ProofOfConcept/Program.cs
+++ planning/proofofconcept/backend/ProofOfConcept/Program.cs
@@ -1,10 +1,16 @@
 
+using Microsoft.AspNetCore.Builder;
 using ProofOfConcept.Hubs;
 
 namespace ProofOfConcept
 {
     public class Program
     {
+        public static readonly Dictionary<string, Type> HubList = new Dictionary<string, Type>()
+        {
+            { "/ws", typeof(TestSignalRHub) }
+        };
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
@@ -14,27 +20,31 @@ namespace ProofOfConcept
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-            });
+            builder.Services.AddSwaggerGen();
             builder.Services.AddSignalR();
+
+#if DEBUG
+            builder.Services.AddSigSpecDocument(o => o.Hubs["/ws"] = typeof(TestSignalRHub));
+#endif
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+#if DEBUG
+            // https://localhost:8729/swagger/v1/swagger.json
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            // https://localhost:8729/sigspec/v1/sigspec.json
+            app.UseSigSpec();
+            app.UseSigSpecUi();
+#endif
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-
             app.MapControllers();
-            app.MapHub<TestSignalRHub>("/ws");
+            app.MapHub<TestSignalRHub>(HubList.First(hl => hl.Value == typeof(TestSignalRHub)).Key);
 
             app.Run();
         }
