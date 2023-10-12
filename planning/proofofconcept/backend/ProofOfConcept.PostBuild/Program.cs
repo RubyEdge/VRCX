9700b3ddc682d5467ae75ea73be8490c42843178
DubyaDude <ushafiq141@gmail.com>
2023-10-11 23:40:19 -0400

    Added code generation for Rest and SignalR

--- planning/proofofconcept/backend/ProofOfConcept.PostBuild/Program.cs
+++ planning/proofofconcept/backend/ProofOfConcept.PostBuild/Program.cs
@@ -1,24 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using NSwag;
+using NSwag.CodeGeneration.TypeScript;
 using SigSpec.CodeGeneration.TypeScript;
 using SigSpec.Core;
 
-Console.WriteLine("Hello, World!");
+string specFolder = @"..\..\specs";
 
-var settings = new SigSpecGeneratorSettings();
-var generator = new SigSpecGenerator(settings);
+{
+    var settings = new SigSpecGeneratorSettings();
+    var generator = new SigSpecGenerator(settings);
 
-// TODO: Add PR to SignalR Core with new IHubDescriptionCollectionProvider service
-var document = await generator.GenerateForHubsAsync(ProofOfConcept.Program.HubList);
+    // TODO: Add PR to SignalR Core with new IHubDescriptionCollectionProvider service
+    var document = await generator.GenerateForHubsAsync(ProofOfConcept.Program.HubList);
 
-var json = document.ToJson();
-Console.WriteLine("\nGenerated SigSpec document:");
-Console.WriteLine(json);
-Console.ReadKey();
+    {
+        var sigSpecJson = document.ToJson();
+        string sigspecFile = $"{specFolder}\\sigspec.json";
+        if (File.Exists(sigspecFile))
+        {
+            File.Delete(sigspecFile);
+        }
+        File.WriteAllText(sigspecFile, sigSpecJson);
+    }
 
-var tsCodeGeneratorSettings = new SigSpecToTypeScriptGeneratorSettings();
-var tsCodeGenerator = new SigSpecToTypeScriptGenerator(tsCodeGeneratorSettings);
-var file = tsCodeGenerator.GenerateFile(document);
+    {
+        var tsCodeGeneratorSettings = new SigSpecToTypeScriptGeneratorSettings();
+        var tsCodeGenerator = new SigSpecToTypeScriptGenerator(tsCodeGeneratorSettings);
+        var tsCodeFile = tsCodeGenerator.GenerateFile(document);
 
-Console.WriteLine("\n\nGenerated SigSpec TypeScript code:");
-Console.WriteLine(file);
-Console.ReadKey();
+        string tsSignalRFile = $"{specFolder}\\API_SIGNALR.ts";
+        if (File.Exists(tsSignalRFile))
+        {
+            File.Delete(tsSignalRFile);
+        }
+        File.WriteAllText(tsSignalRFile, tsCodeFile);
+    }
+}
+
+{
+    var openApiFile = File.ReadAllText($"{specFolder}\\rest_openapi.json");
+    var document = await OpenApiDocument.FromJsonAsync(openApiFile);
+
+    var settings = new TypeScriptClientGeneratorSettings();
+    var generator = new TypeScriptClientGenerator(document, settings);
+    var code = generator.GenerateFile();
+
+    string tsRestApiFile = $"{specFolder}\\API_REST.ts";
+    if (File.Exists(tsRestApiFile))
+    {
+        File.Delete(tsRestApiFile);
+    }
+    File.WriteAllText(tsRestApiFile, code);
+}
+
